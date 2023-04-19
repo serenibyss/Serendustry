6737c5a050aa6cb88db97f9b51b26deafe47e4fb
DStrand1 <dane@strandboge.com>
2023-04-19 11:54:39 -0500

    init

---
+++ src/main/java/dandustry/Dandustry.java
@@ -0,0 +1,14 @@
+package dandustry;
+
+import gregtech.api.GTValues;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Dandustry.ID,
+        name = "Dandustry",
+        version = Dandustry.VERSION,
+        dependencies = GTValues.MOD_VERSION_DEP)
+public class Dandustry {
+
+    public static final String ID = "dandustry";
+    public static final String VERSION = "0.0.1";
+}
